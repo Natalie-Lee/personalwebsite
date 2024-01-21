76b284ad06636b502ff2e04faacf4dbc3aed72c2
Natalie-Lee <chiaenl@andrew.cmu.edu>
2024-01-20 23:10:39 -0800

    adjust footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,8 +12,9 @@ export const Footer = () => {
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
+        <li><a href="#education">Education</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#skill">Skill</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
